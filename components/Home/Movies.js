a82cf012aedb1163343daa8a4fd5bb703e829ae9
ebharathi <elayabharathi10102002@gmail.com>
2023-10-14 00:45:08 +0530

    solved data fetch issue

--- components/Home/Movies.js
+++ components/Home/Movies.js
@@ -26,7 +26,7 @@ const Movies=()=>{
                          console.log("From backend-->",response.data);
                          if(response.data.error==false) 
                          {
-                            setData([...data,response.data.data]);
+                          setData((prevData) => [...prevData, response.data.data]);
                           }
                         })
         }
@@ -34,22 +34,20 @@ const Movies=()=>{
         {
                for(let i=0;i<10;i++)
                {
-                   getDetails(items[i]);  
+                  //  getDetails(items[i]);  
                }
         }
     },[items])
-    useEffect(()=>{
-         console.log("--->",data)
-    },[data])
+  
     return(
       <div>
           {
             data.length>0?
-            <div className="mx-5 mt-5 grid md:grid-cols-6 lg:grid-cols-10 space-x-2 space-y-2">
+            <div className="mx-5 mt-5 grid md:grid-cols-6 lg:grid-cols-10 space-y-5 space-x-3">
                   {
                      data.map((x)=>
                       <div className="cursor-pointer">
-                         <img src={`${x.title.image?x.title.image.url:''}`}/>
+                         <img src={`${x.title.image?x.title.image.url:''}`} style={{height:275}} className="rounded-2xl hover:rounded-sm"/>
                       </div>  
                      )
                   }
